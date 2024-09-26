5c8897afefb2e5d2454ead594d81d10bd9d31c1e
Vasiliy Rubtsov <vasiliy.rubtsov@rtmis.ru>
2024-09-26 11:34:00 +0300

    HW-13. Домашнее задание по теме "Методы объектов"

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String surname;
@@ -15,7 +17,20 @@ public class Author {
         return surname;
     }
 
+    @Override
     public String toString() {
         return this.name + " " + this.surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author author)) return false;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
     }
 }
